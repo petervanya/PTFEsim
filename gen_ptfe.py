f4fb643f80a5522158c138e093b56f7704a5fc03
petervanya <peter.vanya@gmail.com>
2015-12-01 02:04:54 +0000

    Bond construction from topology

--- gen_ptfe.py
+++ gen_ptfe.py
@@ -6,7 +6,7 @@ Arguments:
     <input>                  Input yaml file
 
 Options:
-    --save <fname>           Save the data file
+    --save <fname>           Save the data file
     --parsetopo              Use if parameters should be obtained from topo string
 
 * 5 beads in a monomer
@@ -27,6 +27,7 @@ d_DPD = 8.14e-10             # DPD distance unit
 elem_wts = {"C": 12, "F": 19, "O": 16, "H": 1, "S": 32}
 rho_dry = 1950               # kg/m^3
 rho_wet = 1680
+rho_DPD = 3
 
 def nafion_bead_wt(bead, arg="dry"):
     """Return weigths in atomic units of beads A, B, C or W"""
@@ -69,13 +70,13 @@ def grow_chain(Nm, L, mol_id=1, mu=6.0, sigma=0.6):
     return np.hstack((mol_ids, types, pos))
 
 
-def get_polymer_atoms(Ns, Nm, L, mu, sigma):
+def get_polymer_atoms(Nc, Nm, L, mu, sigma):
     """Generate xyz matrix from a given number of chains"""
-    Nbs = 5*Nm
-    mol_ids = range(1, Ns+1)
-    xyz = np.zeros((Ns*Nbs, 5))
-    for i in range(Ns):
-        xyz[i*Nbs : (i+1)*Nbs] = grow_chain(Nm, L, mol_ids[i], mu, sigma)
+    Nbc = 5*Nm
+    mol_ids = range(1, Nc+1)
+    xyz = np.zeros((Nc*Nbc, 5))
+    for i in range(Nc):
+        xyz[i*Nbc : (i+1)*Nbc] = grow_chain(Nm, L, mol_ids[i], mu, sigma)
     return xyz
 
 
@@ -91,7 +92,7 @@ def get_wb_atoms(Nwb, L, count=1):
 
 def bonds_mat(Na, Nm, Nbm=5):
     """
-    Ns -- num chains
+    Nc -- num chains
     Nm -- num monomers in a chain
     Nbm -- num beads in monomer
     Types:
@@ -101,53 +102,80 @@ def bonds_mat(Na, Nm, Nbm=5):
     Order:
     count, id, part1, part2
     """
-    Nbonds = Ns * (Nm-1 + Nm*4)        # between m'mers + 4 in each m'mer
+    Nbonds = Nc * (Nm-1 + Nm*4)        # between m'mers + 4 in each m'mer
     bonds = np.zeros((Nbonds, 3), dtype=int)
     cnt = 0
-    for i in range(Ns):
-        for j in range(Nm):     # within a monomer
-            first = Ns*i+j+1    # first bead in a monomer
+    for i in range(Nc):
+        for j in range(Nm):            # within a monomer
+            first = Nc*i+j+1           # first bead in a monomer
             bonds[cnt]   = [1, first,   first+1]
             bonds[cnt+1] = [1, first+1, first+2]
             bonds[cnt+2] = [2, first,   first+3]
             bonds[cnt+3] = [3, first+3, first+4]
             cnt += 4
-    for i in range(Ns):
-        firstm = Ns*i+1         # first monomer in str
-        for j in range(Nm-1):   # between monomers
+    for i in range(Nc):
+        firstm = Nc*i + 1              # first monomer in str
+        for j in range(Nm-1):          # between monomers
             bonds[cnt] = [1, firstm+j*Nbm+2, firstm+j*Nbm+5]
             cnt += 1
     return bonds
 
 
-def gen_pair_bond_coeffs(atom_types="ABCW", atoms_yaml, bonds_yaml, gamma, rc, r0):
+def bonds_mat2(raw_topo, Nc):
+    """Create bond matrix from topology string and number of chains"""
+    bead_list, Nm = pt.parse_beads(raw_topo)
+    Nbm = len(bead_list)
+    bonds = pt.construct_bonds(bead_list, Nm, 0)
+    for i in range(1, Nc):
+        bonds = np.vstack( (bonds, pt.construct_bonds(bead_list, Nm, i*Nm*Nbm)) ) 
+    return bonds
+
+
+def gen_pair_coeffs(atom_types, atoms_yaml, gamma, rc):
     """
-    Generate atomic params a_ij and bond coeffs k_ij for
-    all possible combinations give number of atom types
-    Read the custom bonds from the input.yaml file
-    TEST THIS
+    Generate atomic params a_ij for all possible combinations 
+    given number of atom types. Read custom bonds from input.yaml file
+    Nafion atom_types = "ABCW"
     """
     a_ij = {}
-    k_ij = {}
     Nat = len(atom_types)
     num2coeff = dict((num, coeff) for (num, coeff) in zip(range(1, Nat+1), atom_types))
     for i in range(1, Nat+1):
-        for j in range(i+1):
-            key = "%i %i"%(i, j)
-            if num2coeff[key] in atoms_yaml.keys():
-                a_ij[key] = [atoms[num2coeff[key]], gamma, rc]
+        for j in range(1, i+1):
+            key = "%i %i" % (j, i)
+            lkey = "%s %s" % (num2coeff[j], num2coeff[i])
+            if lkey in atoms_yaml.keys() or lkey[::-1] in atoms_yaml.keys():
+                try:
+                    a_ij[key] = [atoms_yaml[lkey] * kB*T/rc, gamma, rc]
+#                    a_ij[key] = [atoms_yaml[lkey], 1.0, 1.0]
+                except KeyError: # "B A" -> "A B"
+                    a_ij[key] = [atoms_yaml[lkey[::-1]] * kB*T/rc, gamma, rc]
+#                    a_ij[key] = [atoms_yaml[lkey[::-1]], 1.0, 1.0]
             else:
                 a_ij[key] = [25 * kB*T/rc, gamma, rc]
-               
-    for i in range(1, Nat+1):
-        for j in range(i+1):
+#                a_ij[key] = [25.0, 1.0, 1.0]
+    return a_ij
+
+
+def gen_bond_coeffs(atom_types, bonds_yaml, r0):
+    """
+    Generate bond coeffs k_ij and r0 for all possible combinations
+    given number of atom types. Read custom bonds from input.yaml file
+    Nafion atom types = "ABCW"
+    """
+    k_ij = {}
+    bmap = pt.bond_map(atom_types)   # "ABCW"
+    Nat = len(atom_types)
+    for i in range(Nat):
+        for j in range(i+1): 
             key = atom_types[i] + " " + atom_types[j]
             if key in bonds_yaml.keys():
-                k_ij[map[key]] = [bonds_yaml[key], r0]
+                k_ij[bmap[key]] = [bonds_yaml[key] * kB*T/(rc**2), r0]
+#                k_ij[bmap[key]] = [bonds_yaml[key], r0]
             else:
-                k_ij[map[key]] = [4 * kB*T/(rc**2), r0]
-
-    return a_ij, k_ij
+                k_ij[bmap[key]] = [4 * kB*T/(rc**2), r0]
+#                k_ij[bmap[key]] = [4, r0]
+    return k_ij
 
 
 def get_electrodes():
@@ -155,9 +183,8 @@ def get_electrodes():
 
 
 # =====
-# ===== printing to str
+# ===== functions to convert stuff to str
 # =====
-# TO DO: FIND BETTER NAMES
 def header2str(N, Nbonds, atomtypes, bondtypes, L):
     """Generate LAMMPS header"""
     s = "#blabla\n"
@@ -186,11 +213,11 @@ def paircoeffs2str(coeffs):   # part1 part2 force gamma cutoff
     return s + "\n"
 
 
-def bondcoeffs2str(id_bondcoeffs):
+def bondcoeffs2str(k_ij):
     """Save bond coefficients into a str"""
     s = "Bond Coeffs\n\n"
-    for k, v in id_bondcoeffs.iteritems():
-        s += str(k) + "\t" + str(v[0]) + "\t" + str(v[1]) + "\n"
+    for k, v in k_ij.iteritems():
+        s += "%s %s %s\n" % (str(k), "%e" % v[0], "%e" % v[1])
     return s + "\n"
 
 
@@ -206,7 +233,7 @@ def atoms2str(atom_mat):
 
 
 def bonds2str(bond_mat):
-    """Convert bond_matrix to str"""
+    """Convert bond matrix to str"""
     M, N = bond_mat.shape
     s = ""
     for i in range(M):
@@ -227,62 +254,66 @@ if __name__ == "__main__":
     # ===== general parameters
     T = data["temperature"]
     L = data["box-size"]*d_DPD
-    atom_types = "ABCW"
+    atom_types = "ABCW"      # MAKE THIS GENERAL
     Nat = len(atom_types)    # num atom types
-    Nbt = 3                  # num bond types
     masses = dict( (i, nafion_bead_wt(i)*Maw) for i in range(1, 5) )  # SI units
     gamma = 1.0 * d_DPD**3   # in SI units, CHECK
     rc = d_DPD               # cutoff distance
+    r0 = data["equilibrium-dist"] * d_DPD
 
     # ===== polymer parameters, MAKE THIS PARSE TOPOLOGY
-    Ns = int(round(num_poly_chains(rho_wet, L**3, Nm, arg="wet")))
     if args["--parsetopo"]:
         raw_topo = data["topology"]
-        bead_list, Nm = pt.bead_parser(raw_topo)
+        bead_list, Nm = pt.parse_beads(raw_topo)
         Nbm = len(bead_list)
-        Nbs = Nbm*Nm
+        Nbc = Nbm*Nm
     else:
         Nm = 15                 # num monomers in one polymer chain
         Nbm = 5                 # num beads per monomer
-        Nbs = Nbm*Nm            # num beads per chain
-    Nb = Nbs*Ns                 # tot num beads
+        Nbc = Nbm*Nm            # num beads per chain
+    print "Nm: %i, Nbm: %i" % (Nm, Nbm)
+    Nc = int(round(num_poly_chains(rho_wet, L**3, Nm, arg="wet")))
+    Nb = Nbc*Nc                 # tot num beads
     mean_bead_dist = L/Nb**(1./3)
     mu = mean_bead_dist
     sigma = mean_bead_dist/10   # 10 is arbitrary
-    print Ns, "polymer chains in a given volume"
+    print Nc, "polymer chains in a given volume"
 
     # ===== pair and bond parameters
-    # NEW AUTOMATISED VERSION: gen_pair_bond_coeffs
-    coeff2num = dict((coeff, num) for (coeff, num) in zip(atom_types, range(1, Nat+1)))
-    a_ij = {}
-    for k, v in data["int-params"].iteritems():
-        a_ij[" ".join([str(coeff2num[i]) for i in k.split()])] \
-            = [v * kB*T/rc, gamma, rc]
-    for i in range(1, Nat+1):   # same bonds
-        a_ij["%i %i" % (i, i)] = [25 * kB*T/rc, gamma, rc]
+    a_ij = gen_pair_coeffs("ABCW", data["pair-coeffs"], gamma, rc)
+    k_ij = gen_bond_coeffs("ABCW", data["bond-coeffs"], r0)
+    Nbt = len(k_ij)                  # num bond types
 
-    k_ij = {}
-    r0 = 0.0
-    for k, v in data["spring-const"].iteritems():
-        k_ij[" ".join([str(coeff2num[i]) for i in k.split()])] \
-            = ["%e" % (v * kB*T/rc**2), r0]
+#    coeff2num = dict((coeff, num) for (coeff, num) in zip(atom_types, range(1, Nat+1)))
+#    a_ij = {}
+#    for k, v in data["pair-coeffs"].iteritems():
+#        a_ij[" ".join([str(coeff2num[i]) for i in k.split()])] \
+#            = [v * kB*T/rc, gamma, rc]
+#    for i in range(1, Nat+1):   # same bonds
+#        a_ij["%i %i" % (i, i)] = [25 * kB*T/rc, gamma, rc]
+#
+#    k_ij = {}
+#    r0 = 0.0
+#    for k, v in data["bond-coeffs"].iteritems():
+#        k_ij[" ".join([str(coeff2num[i]) for i in k.split()])] \
+#            = ["%e" % (v * kB*T/rc**2), r0]
+#    for i in range(1, Nat+1):   # same bonds
+#        k_ij["%i %i" % (i, i)] = ["%e" % (4 * kB*T/rc**2), r0]
 
     # ===== atoms
-    poly_xyz = get_polymer_atoms(Ns, Nm, L, mu, sigma)
-    lmbda = data["membrane"]["lambda"]
-    Nwb = (Ns*Nm*(lmbda-3))/6   # number of water beads
-    wb_xyz = get_wb_atoms(Nwb, L, count=Ns+1)
+    poly_xyz = get_polymer_atoms(Nc, Nm, L, mu, sigma)
+    lmbda = data["water-uptake"]
+    Nwb = (Nc*Nm*(lmbda-3))/6   # number of water beads
+    wb_xyz = get_wb_atoms(Nwb, L, count=Nc+1)
 
     xyz = np.vstack((poly_xyz, wb_xyz))
     xyz_str = atoms2str(xyz)
     print len(xyz), "beads created"
 
     # ===== bonds
-    bonds = bonds_mat(Ns, Nm, Nbm)
+    bonds = bonds_mat2(data["topology"], Nc)  # extracting Nm, Nbm
     bonds_str = bonds2str(bonds)
     print len(bonds), "bonds created"
-    # ALTERNATIVE
-
 
     # ===== putting it together
     final_string = header2str(len(xyz), len(bonds), Nat, Nbt, L) + \
@@ -293,7 +324,7 @@ if __name__ == "__main__":
                    "Bonds\n\n" + bonds_str
 
     if args["--save"]:
-        fname = args["<fname>"]
+        fname = args["--save"]
         open(fname, "w").write(final_string)
         print "Data file written in", fname
     else:
