6c7d7fc553b5567e0de0e8f346ace30d61f2ff43
petervanya <peter.vanya@gmail.com>
2015-12-01 02:03:00 +0000

    Improved naming

--- default_ptfe_input.sh
+++ default_ptfe_input.sh
@@ -7,13 +7,9 @@ box-size:    20        # DPD units, 1 = 8.14 AA
 temperature: 300       # K
 PTFE:        Nafion    # defines beads
 topology:    (A 3, [B 1], [C 1])15
+water-uptake: 9        # water uptake, num H2O/SO3H
 
-membrane:              # Nafion: polymerisation m, num CF2 n
-    m: 15
-    n: 17
-    lambda: 9          # water uptake, num H2O/SO3H
-
-int-params:            # from Wu etal., EES (2008), DPD units of kBT
+pair-coeffs:           # DPD pair coeffs, Wu etal., EES (2008), DPD units of kBT
     A B: 29.0
     A C: 49.3
     A W: 36.0
@@ -21,10 +17,12 @@ int-params:            # from Wu etal., EES (2008), DPD units of kBT
     B W: 30.0
     C W: 29.9
 
-spring-const:
+bond-coeffs:           # spring const param k_ij
     A A: 4.0
     A B: 4.0
     A C: 4.0
+
+equilibrium-dist: 2.0  # r0 in k(r-r0)^2
 
 electrodes:
     width: FILL
