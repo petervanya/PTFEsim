9ed225c1987d23fb1deca892256db1872951ff18
petervanya <peter.vanya@gmail.com>
2015-12-09 16:16:29 +0000

    Added params, explanation

--- default_ptfe_input.sh
+++ default_ptfe_input.sh
@@ -3,11 +3,17 @@
 # 09/11/15
 
 cat >input.yaml <<EOF
-box-size:    20        # DPD units, 1 = 8.14 AA
-temperature: 300       # K
-PTFE:        Nafion    # defines beads
-topology:    (A 3, [B 1], [C 1])15
-water-uptake: 9        # water uptake, num H2O/SO3H
+# ===== notation:
+# W: water bead 6 H2O
+# E: electrodes from carbon
+# P: platinum on electrodes
+# =====
+box-size:       5        # DPD units, 1 = 8.14 AA
+temperature:    300       # K
+PTFE:           Nafion    # defines beads
+topology:       (A 3, [B 1], [C 1])15
+water-uptake:   9         # water uptake, num H2O/SO3H
+gamma:          1.0       # DPD drag coefficient
 
 pair-coeffs:           # DPD pair coeffs, Wu etal., EES (2008), DPD units of kBT
     A B: 29.0
@@ -22,10 +28,10 @@ bond-coeffs:           # spring const param k_ij
     A B: 4.0
     A C: 4.0
 
-equilibrium-dist: 2.0  # r0 in k(r-r0)^2
+equilibrium-dist: 1.0  # r0 in k(r-r0)^2
 
 electrodes:
-    width: FILL
-    Pt-amount: FILL
-    Pt-size: FILL
+    width: 1           # width of one electrode, same on the other side
+    Pt-amount: 0.01    # number of Pt beads per number of carbon black beads
 EOF
+echo "Parameter file saved in input.yaml"
