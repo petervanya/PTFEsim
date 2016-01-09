74319eadb70ead635b18b5def49d3560ea0e2ba9
petervanya <peter.vanya@gmail.com>
2016-01-09 23:38:57 +0000

    Improved UI

--- rdf_water.py
+++ rdf_water.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """Usage:
-    rdf.py <fnames> [--binsize <bs>]
+    rdf.py <fnames> [--bins <nbins>]
 
 Read LAMMPS data files using regex and compute radial distribution function
 for water beads:
@@ -11,26 +11,29 @@ Arguments:
     <fnames>         Regex for all the required xyz files
 
 Options:
-    --binsize <bs>   Size of the bins for the histogram, CAREFUL WITH UNITS [default: 0.05]
+    --bins <nbins>   Number of bins
 
 pv278@cam.ac.uk, 09/01/16
 """
 import numpy as np
 from numpy.linalg import norm
 import matplotlib.pyplot as plt
-from math import pi
+from math import pi, sqrt, log
 import glob, sys
 from docopt import docopt
 import f_rdf      # Fortran module
 
 bins = np.arange(0)
 
-def set_num_bins(N):
-    """Set number of histogram bins, various recipes"""
-    nbins = int(2*N**(1./3)) + 1   # Rice rule
-    nbins = int(sqrt(N)) + 1       # most primitive
-    nbins = int(log(N, 2)+1) + 1   # Sturges' formula, from wiki
-    return nbins
+def set_num_bins(N, method="sqrt"):
+    """Set number of histogram bins, various recipes.
+    Available methods: rice, sturges, sqrt (default)"""
+    if method == "rice":
+        return int(2*N**(1./3)) + 1   # Rice rule
+    elif method == "sturges":
+        return int(log(N, 2)+1) + 1   # Sturges' formula
+    else:
+        return int(sqrt(N)) + 1       # most primitive
 
 
 def read_outfile(outfile):
@@ -54,7 +57,7 @@ def save_data(outfile, *args):
             f.write(line)
 
 
-def compute_rdf(outfile, nbins=nbins):
+def compute_rdf(outfile, nbins=30):
     """Compute radial dist'n fcn from the xyz frame using Fortran routine"""
     global bins
     A = read_outfile(outfile)
@@ -62,23 +65,23 @@ def compute_rdf(outfile, nbins=nbins):
     xyz_W = A[A[:, 0] == 4][:, 1:]
 
     d_C = f_rdf.f_rdf.pair_dist_mat(xyz_C)         # rdf for beads C
-    rdf_raw_C, r = np.histogram(d, bins=nbins)
+    rdf_raw_C, r = np.histogram(d_C, bins=nbins)
     del d_C
     d_W = f_rdf.f_rdf.pair_dist_mat(xyz_W)         # rdf for beads W
     rdf_raw_W, r = np.histogram(d_W, bins=nbins)
     del d_W
-    d_CW = f_rdf.f_rdf.pair_dist_mat(xyz_C, xyz_W) # rdf for combined beads C and W
+    d_CW = f_rdf.f_rdf.pair_dist_mat2(xyz_C, xyz_W) # rdf for combined beads C and W
     rdf_raw_CW, r = np.histogram(d_CW, bins=nbins)
     del d_CW
     
-    rdf_raw = rdf_raw_C * 3**2 + rdf_raw_W * 6**2 + rdf_raw_CW * 3*6
+    rdf_raw = rdf_raw_C * 3**2 + rdf_raw_W * 6**2 + rdf_raw_CW * 3*6
     r = r[:-1] + np.diff(r)/2.0
     dr = r[1] - r[0]
     rdf = rdf_raw/(4*pi*r**2 * dr)
     return r, rdf
 
 
-def master_rdf(outfiles, nbins=nbins):
+def master_rdf(outfiles, nbins=30):
     """Construct an rdf from all the available xyz files"""
     rdf_mat = []
     for outfile in outfiles:
@@ -95,7 +98,6 @@ def master_rdf(outfiles, nbins=nbins):
 if __name__ == "__main__":
     args = docopt(__doc__)
 #    print args
-    dr = float(args["--binsize"])
     outfiles = glob.glob(args["<fnames>"])
     if len(outfiles) == 0:
         print "ERROR: No xyz files found. Aborting."
@@ -104,7 +106,11 @@ if __name__ == "__main__":
     Nfiles = len(outfiles)
     N = int(open(outfiles[0], "r").readline())
     print "Total particles:", N
-    Nbins = set_num_bins(N)
+
+    if args["--bins"]:
+        Nbins = int(args["--bins"])
+    else:
+        Nbins = set_num_bins(N, method="sturges")
     print "Number of bins:", Nbins
 
     r, vals = master_rdf(outfiles, Nbins)
