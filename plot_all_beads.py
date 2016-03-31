8cf94aae7fecfa979e905b49a5529fb7fac247df
petervanya <peter.vanya@gmail.com>
2016-03-31 13:18:21 +0100

    Nicer plots

--- plot_all_beads.py
+++ plot_all_beads.py
@@ -9,12 +9,15 @@ for water, sulfonic acid groups and PTFE backbone
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 import glob, sys
 from docopt import docopt
 
 
 args = docopt(__doc__)
 #print args
+
+matplotlib.rcParams.update({'font.size': 16})
 fileW = args["<fileW>"]
 fileS = args["<fileS>"]
 fileB = args["<fileB>"]
@@ -23,21 +26,22 @@ lw = 2.0    # linewidth
 A = np.loadtxt(fileB)
 A[:, 0] *= 1e9
 A[:, 1] /= sum(A[:, 1])
-plt.plot(A[:, 0], A[:, 1], "red", label="backbone", linewidth=lw)
+plt.plot(A[:, 0], A[:, 1], "red", label="backbone", linewidth=lw+1)
 
-A = np.loadtxt(fileS)
-A[:, 0] *= 1e9
-A[:, 1] /= sum(A[:, 1])
-plt.plot(A[:, 0], A[:, 1], "green", label="sulfonic", linewidth=lw)
+#A = np.loadtxt(fileS)
+#A[:, 0] *= 1e9
+#A[:, 1] /= sum(A[:, 1])
+#plt.plot(A[:, 0], A[:, 1], "green", label="sulfonic", linewidth=lw-1)
 
 A = np.loadtxt(fileW)
 A[:, 0] *= 1e9
 A[:, 1] /= sum(A[:, 1])
-plt.plot(A[:, 0], A[:, 1], "blue", label="water", linewidth=lw)
+plt.plot(A[:, 0], A[:, 1], "blue", label="water", linewidth=lw+1)
 
-plt.xlabel("$x$ (nm)")
-plt.xlim([0.0, 32.0])
+plt.xlabel("$x$ (nm)", fontsize=20)
+plt.xlim([0.5, 32.0])
 plt.yticks([])
+plt.legend(loc="best")
 
 if args["--title"]:
    plt.title(args["<ttl>"])
