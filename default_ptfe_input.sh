3028f98dca9758f36dbc2093d632ba14f54f1411
Peter Vanya <peter.vanya@gmail.com>
2016-06-09 11:42:45 +0100

    Modified into DPD units

--- default_ptfe_input.sh
+++ default_ptfe_input.sh
@@ -3,20 +3,20 @@
 # 09/11/15
 
 cat >input.yaml <<EOF
-# ===== notation:
+# ===== Bead types:
+# 
 # W: water bead 6 H2O
 # E: electrodes from carbon/quartz/silica
 # P: platinum on electrodes
 # =====
-box-size:       40           # DPD units, 1 = 8.14 AA
-temperature:    300          # K
-PTFE:           Nafion       # defines beads
-topology:       (A 3, [B 1], [C 1])15
-num-monomers:   15
-water-uptake:   9            # water uptake, num H2O/SO3H
-gamma:          4.5          # DPD drag coefficient
+box-size:          40           # DPD units, 1 = 8.14 AA
+temperature:       1.0          # Units of kB T
+topology:          (A 3, [B 1], [C 1])15
+mono-per-chain:    15
+water-uptake:      9            # water uptake, number of H2O/SO3H
+gamma:             4.5          # DPD drag coefficient
 
-chi-params:                  # DPD Flory-Huggins params, Wu etal., EES (2008)
+chi-params:                     # DPD Flory-Huggins params (Wu, EES, 2008)
     A B: 1.23
     A C: 7.44
     B C: 2.70
@@ -28,15 +28,16 @@ chi-params:                  # DPD Flory-Huggins params, Wu etal., EES (2008)
     C E: 0.06
     W E: 2.96
 
-bond-coeffs:                 # spring const param k_ij
+bond-coeffs:                    # spring const param k_ij
     A A: 4.0
     A B: 4.0
     A C: 4.0
 
-equilibrium-dist: 0.1        # r0 in k(r-r0)^2
+equilibrium-dist:  0.1          # r0 in k(r-r0)^2
+# spring-const:     4.0
 
 electrodes:
-    width: 5.0               # width of one electrode, same on the other side
-    Pt-amount: 0.0           # number of Pt beads per number of carbon black beads
+    width:         5.0          # width of one electrode, same on the other side
+    Pt-amount:     0.0          # number of Pt beads per number of carbon beads
 EOF
 echo "Parameter file saved in input.yaml"
