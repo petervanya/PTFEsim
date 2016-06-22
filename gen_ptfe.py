f9f3575318a4002b6adda1140e678cd9191ce85c
petervanya <peter.vanya@gmail.com>
2016-06-22 17:57:43 +0100

    Polish

--- gen_ptfe.py
+++ gen_ptfe.py
@@ -265,7 +265,11 @@ if __name__ == "__main__":
     pt_xyz = gen_platinum(Npt, L, Lcl, Lpt)
     xyz = np.vstack((wb_xyz, el_xyz, pt_xyz, poly_xyz))  # careful about order!
     print("%i beads created, density: %.2f" % (len(xyz), len(xyz)/L**3))
+
     atom_ids_l = ["W"]*Nw + ["E"]*Nsup + ["P"]*Npt + list(mono_beads)*Nmc*Nc
+    bt2num = {}
+    for i, bt in enumerate(bead_types): bt2num[bt] = i+1
+    atom_ids_n = [bt2num[bt] for bt in atom_ids_l]
 
     a_ij = gen_inter_coeffs(data["chi-params"], bead_types, gamma, rc=1.0)
     k_ij = {1: [k0, r0]}
@@ -293,13 +297,6 @@ if __name__ == "__main__":
         print("FIELD file saved in %s" % fname)
 
     elif args["lammps"]:
-#        xyz = np.vstack((poly_xyz, wb_xyz, el_xyz, pt_xyz))  # careful about order!
-#        print("%i beads created, density: %.2f" % (len(xyz), len(xyz)/L**3))
-#        atom_ids_l = list(mono_beads)*Nmc*Nc + ["W"]*Nw + ["E"]*Nsup + ["P"]*Npt
-        bt2num = {}
-        for i, bt in enumerate(bead_types): bt2num[bt] = i+1
-        atom_ids_n = [bt2num[bt] for bt in atom_ids_l]
-
         # ===== pair coeffs
         a_ij_lmp = {}
         for k in a_ij.keys():
@@ -309,12 +306,10 @@ if __name__ == "__main__":
             v[1], v[2] = v[2], v[1]  # swap gamma/rc
 
         # ===== molecular ids
-#        mol_ids = []
         Nmol = Nw + Nelb + Nc  # total num of molecules
         mol_ids = list(range(1, Nw+Nelb+1))
         for i in range(Nw+Nelb+1, Nmol+1): #(1, Nc+1):   # chains
             mol_ids += [i]*Nbc
-#        mol_ids += list(range(Nc*Nbc+1, Nmol+1))   # water, support, Pt
 
         xyz_str = ll.atoms2str(\
                   np.hstack((np.matrix(mol_ids).T, np.matrix(atom_ids_n).T, xyz)))
